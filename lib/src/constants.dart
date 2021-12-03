e46dcd4a76bf6ee699c7b7b25a16e5ad897c359e
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2021-12-03 22:50:47 +0200

    feat: implement timers
    - countdown timer
    - adjustable duration
    - customizable interval
    - timers steps switching

---
+++ lib/src/constants.dart
@@ -0,0 +1,8 @@
+const int defaultPomodoroLength = 25;
+const int defaultShortBreakLength = 5;
+const int defaultLongBreakLength = 15;
+
+const bool defaultAutoStartPomodoros = false;
+const bool defaultAutoStartBreaks = false;
+
+const int defaultLongBreakInterval = 4;
--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -3,41 +3,111 @@ import 'package:flutter/material.dart';
 import 'utils/colors.dart';
 
 class PomodorroTheme {
-  static const Color _primaryColor = Color(0xffd95550);
+  static const Color _primaryColor = Color(0xFFCA3C32);
+  static const Color _accentColor = Color(0xFF3F6C33);
 
-  static final MaterialColor _primarySwatch =
-      generateMaterialColor(_primaryColor);
+  static final ColorScheme _lightColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: generateMaterialColor(_primaryColor),
+    accentColor: _accentColor,
+    brightness: Brightness.light,
+  );
+
+  static final ColorScheme _darkColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: generateMaterialColor(_primaryColor),
+    accentColor: _accentColor,
+    brightness: Brightness.dark,
+  );
 
   static const _appBarTheme = AppBarTheme(
     elevation: 0,
   );
 
-  static const TextTheme _lightTextTheme = TextTheme(
-    bodyText1: TextStyle(
+  static const TextTheme _textTheme = TextTheme(
+    headline1: TextStyle(
       fontFamily: 'ArialRounded',
-      fontSize: 16.0,
-      fontWeight: FontWeight.w700,
-      color: Colors.black,
+      fontSize: 96,
+      fontWeight: FontWeight.w300,
+      letterSpacing: -1.5,
     ),
-  );
-
-  static const TextTheme _darkTextTheme = TextTheme(
+    headline2: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 60,
+      fontWeight: FontWeight.w300,
+      letterSpacing: -0.5,
+    ),
+    headline3: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 48,
+      fontWeight: FontWeight.w400,
+    ),
+    headline4: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 34,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25,
+    ),
+    headline5: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 24,
+      fontWeight: FontWeight.w400,
+    ),
+    headline6: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0.15,
+    ),
+    subtitle1: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.15,
+    ),
+    subtitle2: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0.1,
+    ),
     bodyText1: TextStyle(
       fontFamily: 'ArialRounded',
-      fontSize: 16.0,
-      fontWeight: FontWeight.w700,
-      color: Colors.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.5,
+    ),
+    bodyText2: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25,
+    ),
+    button: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 1.25,
+    ),
+    caption: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.4,
+    ),
+    overline: TextStyle(
+      fontFamily: 'ArialRounded',
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.5,
     ),
   );
 
   static ThemeData get light {
     return ThemeData(
       brightness: Brightness.light,
+      colorScheme: _lightColorScheme,
       primaryColor: _primaryColor,
-      splashColor: _primaryColor,
-      primarySwatch: _primarySwatch,
       appBarTheme: _appBarTheme,
-      textTheme: _lightTextTheme,
+      textTheme: _textTheme,
       fontFamily: 'ArialRounded',
     );
   }
@@ -45,11 +115,10 @@ class PomodorroTheme {
   static ThemeData get dark {
     return ThemeData(
       brightness: Brightness.dark,
+      colorScheme: _darkColorScheme,
       primaryColor: _primaryColor,
-      splashColor: _primaryColor,
-      primarySwatch: _primarySwatch,
       appBarTheme: _appBarTheme,
-      textTheme: _darkTextTheme,
+      textTheme: _textTheme,
       fontFamily: 'ArialRounded',
     );
   }
