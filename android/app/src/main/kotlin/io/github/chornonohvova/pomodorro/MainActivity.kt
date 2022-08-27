38e6f13fc7ea9fe52e81cb744e3f50b9834993dc
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2022-08-27 12:52:45 +0300

    feat: migrate to react native

--- android/app/src/main/kotlin/io/github/chornonohvova/pomodorro/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.chornonohvova.pomodorro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/constants.dart
+++
@@ -1,8 +0,0 @@
-const int defaultPomodoroLength = 25;
-const int defaultShortBreakLength = 5;
-const int defaultLongBreakLength = 15;
-
-const bool defaultAutoStartPomodoros = false;
-const bool defaultAutoStartBreaks = false;
-
-const int defaultLongBreakInterval = 4;
