8b04335fa93541727286728f9449115d6701a37f
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2022-09-06 10:31:00 +0300

    fix: update notifications

--- android/app/src/main/java/com/pomodorro/notifications/NotificationHelper.kt
+++ android/app/src/main/java/com/pomodorro/notifications/NotificationHelper.kt
@@ -7,7 +7,9 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
 import android.os.Build
+import android.provider.Settings
 import androidx.core.app.NotificationCompat
 import com.pomodorro.BuildConfig
 import com.pomodorro.MainActivity
@@ -19,19 +21,44 @@ object NotificationHelper {
   private const val POMO_CURRENT_CHANNEL_ID = "${BuildConfig.APPLICATION_ID}.notifications.current"
 
   /**
+   * Create [PendingIntent] for notifications (it'll open [MainActivity])
+   */
+  private fun getContentIntent(context: Context): PendingIntent {
+    val intent = Intent(context, MainActivity::class.java).apply {
+      flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    } else {
+      PendingIntent.getActivity(context, 0, intent, 0)
+    }
+  }
+
+  /**
    * Create notification channel with provided id
    */
-  private fun createChannel(context: Context, channelId: String) {
+  private fun createChannel(
+    context: Context,
+    channelId: String,
+    name: String,
+    descriptionText: String
+  ) {
     // Create the NotificationChannel, but only on API 26+ because
     // the NotificationChannel class is new and not in the support library
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val name = context.getString(R.string.app_name)
-      val descriptionText = context.getString(R.string.app_name)
-
       val importance = NotificationManager.IMPORTANCE_HIGH
+
+      val audioAttributes = AudioAttributes.Builder()
+        .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
+        .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+        .build()
 
       val channel = NotificationChannel(channelId, name, importance).apply {
         description = descriptionText
+        setSound(Settings.System.DEFAULT_NOTIFICATION_URI, audioAttributes)
+        enableLights(true)
+        enableVibration(true)
       }
 
       // Register the channel with the system
@@ -46,22 +73,17 @@ object NotificationHelper {
    */
   @SuppressLint("UnspecifiedImmutableFlag")
   fun getPomoActiveBuilder(context: Context): NotificationCompat.Builder {
-    createChannel(context, POMO_ACTIVE_CHANNEL_ID)
-
-    val intent = Intent(context, MainActivity::class.java).apply {
-      flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-    }
-
-    val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-    } else {
-      PendingIntent.getActivity(context, 0, intent, 0)
-    }
+    createChannel(
+      context,
+      POMO_ACTIVE_CHANNEL_ID,
+      "Pomodorro active",
+      "Notification displayed when pomodorro is started"
+    )
 
     return NotificationCompat.Builder(context, POMO_ACTIVE_CHANNEL_ID).apply {
       priority = NotificationCompat.PRIORITY_MAX
       setSmallIcon(R.drawable.ic_notification)
-      setContentIntent(pendingIntent)
+      setContentIntent(getContentIntent(context))
       setOnlyAlertOnce(true)
       setOngoing(true)
       setShowWhen(false)
@@ -74,23 +96,19 @@ object NotificationHelper {
    */
   @SuppressLint("UnspecifiedImmutableFlag")
   fun getPomoCurrentBuilder(context: Context): NotificationCompat.Builder {
-    createChannel(context, POMO_CURRENT_CHANNEL_ID)
-
-    val intent = Intent(context, MainActivity::class.java).apply {
-      flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-    }
-
-    val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-    } else {
-      PendingIntent.getActivity(context, 0, intent, 0)
-    }
+    createChannel(
+      context,
+      POMO_CURRENT_CHANNEL_ID,
+      "Pomodorro current",
+      "Notification displayed when current focus/break ended, and it's time to start next one"
+    )
 
     return NotificationCompat.Builder(context, POMO_CURRENT_CHANNEL_ID).apply {
       priority = NotificationCompat.PRIORITY_MAX
-      setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+      setDefaults(Notification.DEFAULT_ALL)
       setSmallIcon(R.drawable.ic_notification)
-      setContentIntent(pendingIntent)
+      setContentIntent(getContentIntent(context))
+      setWhen(System.currentTimeMillis())
       setSilent(false)
       setAutoCancel(true)
     }
