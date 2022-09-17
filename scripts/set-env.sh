247b746a05f45f11ab4e6708e894219bfefca7a8
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2022-09-17 13:54:34 +0300

    ci: update set env script release version parsing

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-regex='^(v[0-9\.]{5})-(alpha|beta|internal)$'
+regex='^v[0-9\.]+(-(alpha|beta|internal))*[\.0-9]*$'
 
-version=$(echo -n "$1" | sed -En "s/$regex/\1/p")
 track=$(echo -n "$1" | sed -En "s/$regex/\2/p")
 
-version=$([[ -z "$version" ]] && echo -n "$1" || echo -n "$version")
 track=$([[ -z "$track" ]] && echo -n "production" || echo -n "$track")
 
 echo "Settings env variables"
-echo "version: $version"
-echo "track: $track"
+echo "RELEASE_VERSION: $1"
+echo "RELEASE_TRACK: $track"
 
-echo "version=$version" >> $GITHUB_ENV
-echo "track=$track" >> $GITHUB_ENV
+echo "RELEASE_VERSION=$1" >> $GITHUB_ENV
+echo "RELEASE_TRACK=$track" >> $GITHUB_ENV
